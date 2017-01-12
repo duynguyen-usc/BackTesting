bc6841dfb0a441d9834ba0c6bd777bdf43ace538
Duy Nguyen <salesnguyen1@gmail.com>
2017-01-11 17:40:41 -0800

    Add basic daily calculations

--- getData.py
+++ getData.py
@@ -1,33 +1,40 @@
 from datetime import datetime
 
+ROUND_PRECISION = 4
 
-# 0-Date,1-Open,2-High,3-Low,4-Close,5-Volume,6-Adj Close	
 def getDate(csvLine):	
 	return csvLine.split(',')[0]
 
 def getOpenPrice(csvLine):	
-	return csvLine.split(',')[1]
+	return float(csvLine.split(',')[1])
 
 def getHighPrice(csvLine):	
-	return csvLine.split(',')[2]
+	return float(csvLine.split(',')[2])
 
 def getClosePrice(csvLine):	
-	return csvLine.split(',')[3]
+	return float(csvLine.split(',')[3])
 
 def getClosePrice(csvLine):	
-	return csvLine.split(',')[4]
+	return float(csvLine.split(',')[4])
 
 def getVolume(csvLine):	
-	return csvLine.split(',')[5]
+	return float(csvLine.split(',')[5])
 
 def getAdjClosePrice(csvLine):	
-	return csvLine.split(',')[6]
+	return float(csvLine.split(',')[6])
 
+def netChange(dataSet1, dataSet2):
+	return round(getClosePrice(dataSet2) - getClosePrice(dataSet1), ROUND_PRECISION)
 
+def percentChange(dataSet1, dataSet2):
+	return round((netChange(dataSet1,dataSet2) / getClosePrice(dataSet1) * 100), ROUND_PRECISION)
+
+def dayRange(dataSet):
+	return round(getClosePrice - getOpenPrice(dataSet), ROUND_PRECISION)
+	
 # Main
 
-csvData = [line.rstrip('\n') for line in open('Data\SPX.csv')]
-for idx, val in enumerate(csvData):
-	print(getDate(val) + ' = ' + getClosePrice(val))
-	
-	
+SpxData = [line.rstrip('\n') for line in open('Data\Test.csv')]
+for idx, val in enumerate(SpxData):
+	if(idx != 0) and (idx < len(SpxData)-1):		
+		print(percentChange(SpxData[idx + 1], val))
