c8411a3281a3e9e11a643ac71f361c1c69792623
Duy <duy@duys-mbp.home>
2017-01-20 14:40:19 -0800

    Calculate net change

--- EquityData.py
+++ EquityData.py
@@ -3,21 +3,24 @@ from PriceData import PriceData
 class EquityData:
 
 	ROUND_PERCISION = 2
-	MOV_AVG = [5,10]
+	MOV_AVG = [20,50, 100, 200, 300]
 
 	def __init__(self, csvFile):
 		self.allData = []
 		self.parseCsvFile(csvFile)
-		self.calcMovAvgs()
+		self.runInterDayCalculations()
 
 	def parseCsvFile(self, csvFile):
 		data = [line.rstrip('\n') for line in open(csvFile)]
 		for idx, csvline in enumerate(data):
 			if(idx != 0):		
-				self.allData.append((PriceData(csvline)))	
+				self.allData.append((PriceData(csvline)))
 
-	def calcMovAvgs(self):
+	def runInterDayCalculations(self):
 		for idx, priceData in reversed(list(enumerate(self.allData))):
+			if(idx < len(self.allData) - 1):
+				priceData.netChange = priceData.close - self.allData[idx + 1].close
+
 			for period in self.MOV_AVG:
 				if(idx > period):
 					periodStart = idx - period
@@ -27,6 +30,7 @@ class EquityData:
 
 	def displayData(self):
 		for day in self.allData:
-			print("{0} {1} {2}".format(str(day.date.strftime('%m/%d/%Y')), 
-									   str(round(day.close, self.ROUND_PERCISION)), 
-									   str(round(day.movAvg[len(day.movAvg)-1], self.ROUND_PERCISION))))
+			print("{0} {1} {2} {3}".format(str(day.date.strftime('%m/%d/%Y')), 
+								  	       str(round(day.close, self.ROUND_PERCISION)),
+									       str(round(day.netChange, self.ROUND_PERCISION)), 
+									       str(round(day.movAvg[len(day.movAvg)-1], self.ROUND_PERCISION))))
