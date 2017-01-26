9a67af4ab94924d9f9a06a30b30ad216303a219b
Duy <duy@duys-mbp.home>
2017-01-25 22:38:50 -0800

    Run calculations on all files in Data

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(path)
 datadir = 'Data/'
 csvfiles=[]
-for file in os.listdir(datadir):
-    if file.endswith(".csv"):
-        csvfiles.append(file)
-
-for f in csvfiles:
-	print(f)
-	historicalData = EquityData(datadir + f)
-	historicalData.trendStats(EquityData.TWO_HUNDRED_DAY)
+for f in os.listdir(datadir):
+    if f.endswith(".csv"):
+    	print(f)
+    	historicalData = EquityData(datadir + f)
+    	historicalData.trendStats(EquityData.TWO_HUNDRED_DAY)	
