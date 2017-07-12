fcb1712dd0e4869a164a75b6204198414b41c4ca
Duy Nguyen <salesnguyen1@gmail.com>
2017-07-12 11:50:51 -0700

    refactor for bull put

--- Option.py
+++ Option.py
@@ -1,82 +1,47 @@
 
-class Option:
-	SHORT = 0
-	LONG = 1	
-	VERTICAL_PUT = 2
-	VERTICAL_CALL = 3
+class Option:	
+	SHORT_VERTICAL_PUT = 0
+	LONG_VERTICAL_CALL = 1
 
 	PCT_DOWN = 0.07
 	PCT_UP = 0.015
 
-	def __init__(self, pos, opt, daydata):		
-		self.d = daydata # PriceData
+	def __init__(self, optstruct, today):		
+		self.today = today # PriceData
 		self.longstrike = None
-		self.shortstrike = None
-		self.__setposition(pos)
+		self.shortstrike = None		
 		self.__setoptstructure(opt)
 		self.__setStrikes()
 
-	def __setposition(self, p):
-		if(p not in (self.SHORT, self.LONG)):
-			raise ValueError('position not valid')
-		self.pos = p
-
-	def __setoptstructure(self, o):
-		if(o not in (self.VERTICAL_PUT, self.VERTICAL_CALL)):
+	def __setoptstructure(self, optstruct):
+		if(optstruct not in (self.SHORT_VERTICAL_PUT, 
+					 		 self.VERTICAL_CALL)):
 			raise ValueError('optionstructure not valid')
-		self.structure = o
+		self.structure = optstruct
 
-	def __setspread(self):
-		if (self.d.close >= 100): 
-			spread = 20 
-		else: 
-			spread = 5
-		if (self.longstrike == None):
-			if (self.pos == self.SHORT):
-				self.longstrike = self.shortstrike - spread
-			else:
-				self.longstrike = self.shortstrike + spread
-		else:			
-			if (self.pos == self.SHORT):
-				self.shortstrike = self.longstrike + spread
-			else:
-				self.shortstrike = self.longstrike - spread
+	def __getspread(self):
+		return 20 if (self.today.close >= 100) else 5
+
+	def __setLegs(self):
+		if (self.__isBullPut()):
+			self.longstrike = self.shortstrike - self.__getspread()		
 
 	def __roundStrike(self, x, base=5):		
 		return int(base * round(float(x) / base))
 
 	def __setStrikes(self):
-		if(self.__isShortPutVertical()):						
-			self.shortstrike = self.__roundStrike(self.d.close * (1 - self.PCT_DOWN))
-		else:
-			self.longstrike = self.__roundStrike(self.d.close * (1 + self.PCT_UP))
-		
-		self.__setspread()
+		if (self.__isBullPut()):						
+			self.shortstrike = self.__roundStrike(self.today.close * (1 - self.PCT_DOWN))		
+		self.__setLegs()		
 
-	def __isShortPutVertical(self):
-		return (self.pos == self.SHORT and self.structure == self.VERTICAL_PUT)
-
-	def __isShortCallVertical(self):
-		return (self.pos == self.SHORT and self.structure == self.VERTICAL_CALL)
-
-	def __isLongPutVertical(self):
-		return (self.pos == self.LONG and self.structure == self.VERTICAL_PUT)
-
-	def __isLongCallVertical(self):
-		return (self.pos == self.LONG and self.structure == self.VERTICAL_CALL)
+	def __isBullPut(self):
+		return self.structure == self.SHORT_VERTICAL_PUT
 
 	def __isWin(self, expclose):
-		if (self.__isShortPutVertical()):
+		if (self.__isBullPut()):
 			return expclose > self.shortstrike
-
-		elif (self.__isLongPutVertical()):
-			return expclose < longstrike
-
-		elif (self.__isShortCallVertical()):
-			return expclose < shortstrike
-
-		elif (self.__isLongCallVertical()):
-			return expclose > longstrike
+		return False
+		
 
 	def __isMaxLoss(self, expclose):
 		if(not self.isWin(expclose)):
