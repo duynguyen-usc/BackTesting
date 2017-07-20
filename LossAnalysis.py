5fdc2c8abe67d58c2e1df7827e7def01ac8c2308
Duy Nguyen <salesnguyen1@gmail.com>
2017-07-19 17:24:35 -0700

    add verbose option

--- LossAnalysis.py
+++ LossAnalysis.py
@@ -13,9 +13,10 @@ class LossAnalysis:
 	def __daysInTheMoney(self):
 		return sum([1 for d in self.hpdata if self.__isInTheMoney(d)])
 
-	def toString(self):
-		strLa = "{0}\t itm:{1}\n".format(self.option.toString(), self.__daysInTheMoney())
-		for day in self.hpdata:
-			itm = 'x' if self.__isInTheMoney(day) else ''
-			strLa += "{0}\t{1}\n".format(day.toString(), itm)
+	def toString(self, verbose=False):
+		strLa = "{0}\titm:{1}".format(self.option.toString(), self.__daysInTheMoney())
+		if (verbose):
+			for day in self.hpdata:
+				itm = 'x' if self.__isInTheMoney(day) else ''
+				strLa += "{0}\t{1}\n".format(day.toString(), itm)
 		return strLa
