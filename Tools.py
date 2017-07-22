a1168a2039b7863774b5b0581c2f8a01698abbee
Duy <duy@duys-mbp.home>
2017-07-21 19:24:14 -0700

    format date

--- Tools.py
+++ Tools.py
@@ -6,13 +6,16 @@ class StringBuilder:
 		self.s += "{0}".format(s)
 
 	def addline(self, s):
-		self.s += s + '\n'
+		self.s += "{0}\n".format(s)
 
 	def addtab(self, s):
-		self.s += s + '\t'
+		self.s += "{0}\t".format(s)
+
+	def addDate(self, d):
+		self.s += "{0}\t".format(d.strftime('%Y-%m-%d'))
 
 	def prepend(self, s):
-		self.s = s + self.s
+		self.s = "{0}{1}".format(s, self.s)
 
 	def toString(self):
 		return self.s
